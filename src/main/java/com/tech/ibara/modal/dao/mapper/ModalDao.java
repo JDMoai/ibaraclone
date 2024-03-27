d0acd07510c15038379364b6d7d745ac519750b0
JDMoai <ehdrlf0401@naver.com>
2024-03-27 15:47:33 +0900

    p8-3 오류는 해결했지만 여전히 db데이터내용은 안나오는중

--- src/main/java/com/tech/ibara/modal/dao/mapper/ModalDao.java
+++ src/main/java/com/tech/ibara/modal/dao/mapper/ModalDao.java
@@ -2,11 +2,9 @@ package com.tech.ibara.modal.dao.mapper;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-
 import com.tech.ibara.modal.dto.ModalCheckDto;
 
-@Repository
+
 public interface ModalDao {
 	List<ModalCheckDto> selectServiceItems();
 }
